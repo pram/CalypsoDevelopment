ff450978ab2c5a671471c2e293b6bb873fb77b2f
pram <pram@attale.com>
2012-12-19 16:56:50 +0200

    Converted mache to a maven project to ease reuse

---
+++ MaCHe/src/test/groovy/com/naughtyzombie/mache/SettingsTest.groovy
@@ -0,0 +1,17 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.naughtyzombie.mache
+
+import com.naughtyzombie.mache.Settings
+
+
+/**
+ * Tests for the {@link Settings} class.
+ */
+class SettingsTest extends GroovyTestCase {
+    void testHelp() {
+
+    }
+}
---
+++ MaCHe/src/test/groovy/com/naughtyzombie/mache/WorkerTest.groovy
@@ -0,0 +1,16 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.naughtyzombie.mache
+
+import com.naughtyzombie.mache.Worker
+
+/**
+ * Tests for the {@link Worker} class.
+ */
+class WorkerTest extends GroovyTestCase {
+    void testShow() {
+
+    }
+}
