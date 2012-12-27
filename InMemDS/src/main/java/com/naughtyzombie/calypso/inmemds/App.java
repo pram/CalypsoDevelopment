4b3d2ac4651128d5e7b930d7d4804f2748fb223d
Pram Attale <pram@attale.com>
2012-12-27 19:22:26 +0000

    Adding new InMemDS project

---
+++ InMemDS/src/main/java/com/naughtyzombie/calypso/inmemds/App.java
@@ -0,0 +1,13 @@
+package com.naughtyzombie.calypso.inmemds;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
